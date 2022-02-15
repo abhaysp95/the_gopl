81301ed584ab5ee96be2d56ea071bef9ca76698b
coolabhays <abhaysp9955@gmail.com>
2022-02-15 16:11:22 +0530

    pretty print Tracks

--- ch7/sorting/main.go
+++ ch7/sorting/main.go
@@ -24,3 +24,15 @@ func length(s string) time.Duration {
   }
   return d
 }
+
+// print in Tabular form
+func printTracks(tracks []*Track) {
+  const format = "%v\t%v\t%v\t%v\t%v\t\n"
+  tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+  fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+  fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+  for _, t := range tracks {
+    fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+  }
+  tw.Flush()
+}
