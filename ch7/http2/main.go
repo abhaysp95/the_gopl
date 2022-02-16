3c2bd5e544408931c369061b4410c2f75634ab3f
coolabhays <abhaysp9955@gmail.com>
2022-02-16 13:16:02 +0530

    preperation for new interface logic

---
+++ ch7/http2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+)
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func main() {
+  db := database{"shoes": 50, "socks": 5}
+  http.ListenAndServe("localhost:8000", db)
+}
