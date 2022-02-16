f2a34fac11e064af5fee0119c91381b30e60d03e
coolabhays <abhaysp9955@gmail.com>
2022-02-16 17:41:56 +0530

    add main function

--- ch7/http3/main.go
+++ ch7/http3/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 type dollar float32
@@ -10,3 +11,11 @@ type dollar float32
 func (d dollar) String() string { return fmt.Sprintf("$%.2f", d) }
 
 type database map[string]dollar
+
+func main() {
+  db := database{"shoes": 50, "socks": 5}
+  mux := http.NewServeMux()
+  mux.HandleFunc("/list", http.HandlerFunc(db.list))
+  mux.HandleFunc("/price", http.HandlerFunc(db.price))
+  log.Fatal(http.ListenAndServe("localhost:8000", mux))
+}
