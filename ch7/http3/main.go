ed07933d4e3d4881edac44bb41b6b5ea1143f38a
coolabhays <abhaysp9955@gmail.com>
2022-02-16 17:58:07 +0530

    fix mistake, use Handle, not HandlerFunc

--- ch7/http3/main.go
+++ ch7/http3/main.go
@@ -34,7 +34,7 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 func main() {
   db := database{"shoes": 50, "socks": 5}
   mux := http.NewServeMux()
-  mux.HandleFunc("/list", http.HandlerFunc(db.list))  // type conversion
-  mux.HandleFunc("/price", http.HandlerFunc(db.price))
+  mux.Handle("/list", http.HandlerFunc(db.list))  // type conversion
+  mux.Handle("/price", http.HandlerFunc(db.price))
   log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
