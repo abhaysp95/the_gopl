38cda3f1c1f5f6ccbcd9e040e5974129920e5a5b
coolabhays <abhaysp9955@gmail.com>
2022-02-16 17:49:57 +0530

    add handler functions for patterns

--- ch7/http3/main.go
+++ ch7/http3/main.go
@@ -12,6 +12,25 @@ func (d dollar) String() string { return fmt.Sprintf("$%.2f", d) }
 
 type database map[string]dollar
 
+// to be used via http.HandlerFunc (adapter to allow the use of ordinary
+// functions as HTTP Handler)
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+  for item, value := range db {
+    fmt.Fprintf(w, "%s: %s\n", item, value)
+  }
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+  item := req.URL.Query().Get("item")
+  price, ok := db[item]
+  if !ok {
+    w.WriteHeader(http.StatusNotFound)
+    fmt.Fprintf(w, "no such item: %q\n", item)
+    return
+  }
+  fmt.Fprintf(w, "%s\n", price)
+}
+
 func main() {
   db := database{"shoes": 50, "socks": 5}
   mux := http.NewServeMux()
