fa49edd719a7672240c534a9b446711471a887d4
coolabhays <abhaysp9955@gmail.com>
2022-02-16 13:53:13 +0530

    create Handler for db database

--- ch7/http2/main.go
+++ ch7/http2/main.go
@@ -12,6 +12,28 @@ func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
 
 type database map[string]dollars
 
+func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+  switch req.URL.Path {
+  case "/list":
+    for item, value := range db {
+      fmt.Fprintf(w, "%s: %s\n", item, value)
+    }
+  case "/price":
+    item := req.URL.Query().Get("item")
+    // fmt.Println(url.ParseQuery(req.URL.Query().Encode()))
+    price, ok := db[item]
+    if !ok {
+      w.WriteHeader(http.StatusNotFound)
+      fmt.Fprintf(w, "no such item: %q\n", item)
+      return
+    }
+    fmt.Fprintf(w, "%s: %s\n", item, price)
+  default:
+    w.WriteHeader(http.StatusNotFound)  // 404
+    fmt.Fprintf(w, "no such page: %s\n", req.URL)
+  }
+}
+
 func main() {
   db := database{"shoes": 50, "socks": 5}
   http.ListenAndServe("localhost:8000", db)
