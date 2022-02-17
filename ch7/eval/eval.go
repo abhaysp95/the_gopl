20a09eac89e019e5a60dedf532d0f968616e510e
coolabhays <abhaysp9955@gmail.com>
2022-02-17 17:38:25 +0530

    concrete Eval methods (becoming Expr)

---
+++ ch7/eval/eval.go
@@ -0,0 +1,53 @@
+package eval
+
+import (
+	"fmt"
+	"math"
+)
+
+// map variable names to values
+type Env map[Var]float64
+
+func (v Var) Eval(env Env) float64 {
+  return env[v]
+}
+
+func (l literal) Eval(_ Env) float64 {
+  return float64(l)
+}
+
+func (u unary) Eval(env Env) float64 {
+  switch u.op {
+  case '+':
+    return +u.x.Eval(env)
+  case '_':
+    return -u.x.Eval(env)
+  }
+  panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (b binary) Eval(env Env) float64 {
+  switch b.op {
+  case '+':
+    return b.x.Eval(env) + b.y.Eval(env)
+  case '-':
+    return b.x.Eval(env) - b.y.Eval(env)
+  case '*':
+    return b.x.Eval(env) * b.y.Eval(env)
+  case '/':
+    return b.y.Eval(env) / b.y.Eval(env)
+  }
+  panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+
+func (c call) Eval(env Env) float64 {
+  switch c.fn {
+  case "pow":
+    return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+  case "sin":
+    return math.Sin(c.args[0].Eval(env))
+  case "sqrt":
+    return math.Sqrt(c.args[0].Eval(env))
+  }
+  panic(fmt.Sprintf("unsupported function call: %q", c.fn))
+}
