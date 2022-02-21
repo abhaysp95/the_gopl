8a2f84bee089b17a25278778b71e891791b06530
coolabhays <abhaysp9955@gmail.com>
2022-02-21 16:10:50 +0530

    update client so that it may send terminal input to server and print server response to terminal

---
+++ ch8/netcat2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	go mustCopy(os.Stdout, conn)
+	mustCopy(conn, os.Stdin)
+}
+
+func mustCopy(dest io.Writer, src io.Reader) {
+	// fmt.Printf("%T, %[1]v\n", src)  // just for info
+	if _, err := io.Copy(dest, src); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("End:", src)
+}
