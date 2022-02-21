453f68a13da81038f2ce4f0a5d51c67d54dab6f6
coolabhays <abhaysp9955@gmail.com>
2022-02-21 12:02:07 +0530

    changed indentation

--- ch8/clock1/main.go
+++ ch8/clock1/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net"
-	"time"
+    "io"
+    "log"
+    "net"
+    "time"
 )
 
 func main() {
-  listener, err := net.Listen("tcp", "localhost:8000")
-  if err != nil {
-    log.Fatal(err)
-  }
-  for {
-    conn, err := listener.Accept()
+    listener, err := net.Listen("tcp", "localhost:8000")
     if err != nil {
-      log.Print(err)  // e.g., connection aborted
-      continue
+        log.Fatal(err)
     }
-    handleConn(conn)  // handle one connection at a time
-  }
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            log.Print(err)  // e.g., connection aborted
+            continue
+        }
+        handleConn(conn)  // handle one connection at a time
+    }
 }
 
 func handleConn(c net.Conn) {
-  defer c.Close()
-  for {
-    // if first argument satisfies StringWriter interface, its WriteString
-    // method is called, else Write is called exactly one
-    _, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
-    if err != nil {
-      return  // e.g., client disconnected
+    defer c.Close()
+    for {
+        // if first argument satisfies StringWriter interface, its WriteString
+        // method is called, else Write is called exactly one
+        _, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+        if err != nil {
+            return  // e.g., client disconnected
+        }
+        time.Sleep(1 * time.Second)
     }
-    time.Sleep(1 * time.Second)
-  }
 }
